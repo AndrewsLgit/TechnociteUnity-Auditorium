555aefe9ad63c48bcf91010c080c0c3565c98e66
13459_gamedev (Student) <s_13459_gamedev@PEDAGOGIQUE.LAN>
2025-04-30 11:54:46 +0200

    fixed effector

--- Auditorium/Assets/_/Features/Game/Effector.cs
+++ Auditorium/Assets/_/Features/Game/Effector.cs
@@ -8,6 +8,11 @@ public class EffectorScript : MonoBehaviour
     private IAuditoriumInputController _inputController;
     private Ray _ray;
     private Camera _camera;
+    private bool _mouseOverBorder = false;
+    private bool _isDragging = false;
+    private Vector3 _previousScale;
+    [SerializeField] private float _minScale = 0.4f;
+    [SerializeField] private float _maxScale = 1.2f;
 
     #endregion
     
@@ -19,6 +24,9 @@ public class EffectorScript : MonoBehaviour
         _inputController = GetComponent<IAuditoriumInputController>();
         _camera = Camera.main;
         SubscribeToAllEvents();
+        _previousScale = transform.localScale;
+        _maxScale = transform.localScale.x * 2;
+        _minScale = transform.localScale.x/3;
     }
 
     private void OnDestroy()
@@ -36,17 +44,45 @@ public class EffectorScript : MonoBehaviour
     {
         
     }
-    
+
+    private void OnMouseOver()
+    {
+        float newRadius = Vector2.Distance(transform.position, GetMouseWorldPoint());
+        
+            if (newRadius >= _maxScale || newRadius <= _minScale)
+            {
+                _mouseOverBorder = false;
+                
+            }
+            else
+            {
+                _mouseOverBorder = true;
+                var newScale = new Vector3(_previousScale.x + 0.1f, _previousScale.y + 0.1f, 0);
+                transform.localScale = (newScale.x >= _maxScale) || (newScale.x <= _minScale) ? newScale : _previousScale;
+                //transform.localScale = new Vector3(_previousScale.x + 0.1f, _previousScale.y + 0.1f, 0);
+            }
+            
+            Debug.Log(_mouseOverBorder);
+    }
+
+    private void OnMouseDrag()
+    {
+        if (_mouseOverBorder)
+        {
+            float newRadius = Vector2.Distance(transform.position, GetMouseWorldPoint());
+            float clampedScale = Mathf.Clamp(newRadius * 2f, _minScale, _maxScale);
+            
+            transform.localScale = new Vector3(clampedScale, clampedScale, 1f);
+            _previousScale = transform.localScale;
+            
+        }
+        else transform.position = GetMouseWorldPoint();
+    }
+
     #endregion
     
     #region Main Methods
-
-    private Vector3 LookMousePosition()
-    {
-        var pos = GetMousePosition();
-        return _camera.ScreenToWorldPoint(new Vector3(pos.x, pos.y, _camera.transform.position.z));
-    }
-
+    
     private void OnClickEnd()
     {
         /*Debug.Log(LookMousePosition().ToString());*/
@@ -54,14 +90,21 @@ public class EffectorScript : MonoBehaviour
 
     private void OnClickStart()
     {
-        Debug.Log(LookMousePosition().ToString());
+        
+        Debug.Log(GetMouseWorldPoint().ToString());
     }
     
     #endregion
 
     #region Utils
 
-    private Vector3 GetMousePosition() => _inputController.MousePosition;
+    private Vector2 GetMousePosition2D() => _inputController.MousePosition;
+    
+    private Vector2 GetMouseWorldPoint() => _camera.ScreenToWorldPoint(GetMousePosition2D());
+    /*{
+        var pos = (Vector2)GetMousePosition();
+        return _camera.ScreenToWorldPoint(pos);
+    }*/
 
     private void UnSubscribeFromAllEvents()
     {
@@ -75,7 +118,6 @@ public class EffectorScript : MonoBehaviour
         _inputController.SubToClickEndEvent(OnClickEnd);
         
     }
-
 
     #endregion
     
